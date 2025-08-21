c7b857c1a10cea35b8f33307061fe57c0b83bf44
BalandinaM <marina.balandina.89@mail.ru>
2025-08-21 23:50:06 +0500

    Реализована отрисовка каталога товаров

--- src/catalog/catalog.jsx
+++ src/catalog/catalog.jsx
@@ -1,8 +1,39 @@
-import { useLoaderData } from 'react-router-dom'
+import { NavLink, useLoaderData } from 'react-router-dom'
 
 export const Catalog = () => {
-  const productsArray = useLoaderData();
-  console.log(productsArray);
+  const products = useLoaderData();
 
-  return <div>Каталог</div>;
+  //вынести карточку в отдельный компонент
+  //подумать над неймингом, ерунду какая-то
+  const productsForRender = products.map((item) => {
+    const productForRender = item.colors.map((elem) => {
+      console.log(elem);
+      return (
+        <li key={`${item.id} + ${elem.id}`} className='products_item'>
+          <NavLink to={`/product/${item.id}/${elem.id}`}>
+              <h3>
+                {item.name} {elem.name}
+              </h3>
+              <img
+                src={elem.images[0]}
+                alt={`${item.name} ${elem.name}`}
+                width={50}
+                height={75}
+              />
+              {/* <p>{elem.description}</p> */}
+              <p>Цена: {elem.price}</p>
+          </NavLink>
+        </li>
+      );
+    });
+    console.log(productForRender);
+    return productForRender;
+  });
+
+  return (
+    <div>
+      <h2>Каталог</h2>
+      <ul className='products_list'>{productsForRender}</ul>
+    </div>
+  );
 };
