4e1ed3c5f9bdba8168c0b2eb392efd6c4bddf866
BalandinaM <marina.balandina.89@mail.ru>
2025-09-07 22:16:22 +0500

    Добавлена возможность удалить товар из корзины. Стили корзины

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,29 +1,46 @@
-import { useContext, useEffect, useState } from "react"
-import { CartContext } from "../../CartContext"
+import { useContext, useEffect, useState } from "react";
+import { CartContext } from "../../CartContext";
 
 export const Cart = () => {
   const cart = useContext(CartContext);
   const [productsInCart, setProductsCart] = useState([]);
 
   useEffect(() => {
-    const loadProducts = async() => {
+    const loadProducts = async () => {
       if (cart.items.length > 0) {
         const result = await cart.getProductsForCart(cart.items);
         setProductsCart(result);
-    }
-  };
-  loadProducts();
- }, [cart]);
-
- console.log(cart.items)
- console.log(productsInCart);
+      } else {
+        setProductsCart([]);
+      }
+    };
+    loadProducts();
+  }, [cart, cart.items]);
 
   return (
     <div>
-      <ul>
+      <ul className="cart_list">
         {productsInCart.map((item) => (
-          <li key={item.id}>
-            Товар {item.name}
+          <li className="cart_item" key={item.cartId} >
+            <img
+              src={item.image}
+              alt={`${item.description}`}
+              width={50}
+              height={75}
+            />
+            <h3>{item.description}</h3>
+            <p>
+              <span>{item.sizeLabel}</span> 
+              <span> ({item.sizeNum})</span>
+            </p>
+            <p>{item.price} руб.</p>
+            <button
+              onClick={() => {
+                cart.remove(item.cartId);
+              }}
+            >
+              Удалить товар из корзины
+            </button>
           </li>
         ))}
       </ul>
